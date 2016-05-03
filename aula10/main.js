af86bbf8e8e4dd05de48da164ab6eb3c863a592b
Nuno Namorado <n.namorado@gmail.com>
2016-05-03 20:14:58 +0100

    Fim aula 10

--- aula10/main.js
+++ aula10/main.js
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+function sayHello(name) {
+    var message = "Olá, ";    
+    var completeMessage = message + name + " e amigos!!!!!";    
+    
+    var p = document.createElement("p");
+    
+    var text = document.createTextNode(completeMessage);
+    p.appendChild(text);
+    
+    var container = document.querySelector(".container");
+    container.appendChild(p);
+};
+
+
+sayHello("Sara");
+sayHello("Manuel");
+sayHello("João");
+sayHello("Joana");
+sayHello("Nora");
+sayHello("Nuno");
---
+++ aula10/structure.js
@@ -0,0 +1,6 @@
+var container = document.querySelector('.container');
+
+var labelNameElement = document.createElement("label");
+labelNameElement.appendChild(document.createTextNode("Nome"));
+
+container.appendChild(labelNameElement);
